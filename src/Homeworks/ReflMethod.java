982d7b163bbfa09702d04534353a8ef7782c5bd2
zakariannn <72627891+zakariannn@users.noreply.github.com>
2021-08-04 04:16:58 +0400

    Initial commit

---
+++ src/Homeworks/ReflMethod.java
@@ -0,0 +1,46 @@
+package Homeworks;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+public class ReflMethod {
+
+
+    public static Object CopyObject(Object object)
+    {
+        try
+        {
+            Object clone = object.getClass().newInstance();
+
+            for(Field field : object.getClass().getDeclaredFields()) {
+                field.setAccessible(true);
+
+                if(field.get(object) == null || Modifier.isFinal(field.getModifiers()))
+                {
+                    continue;
+                }
+
+                if(field.getType().isPrimitive()
+                        || field.getType().equals(String.class)
+                        || field.getType().getSuperclass().equals(Number.class)
+                        || field.getType().equals(Boolean.class))
+                {
+                    field.set(clone, field.get(object));
+                }
+                else {
+                    Object childObj = field.get(object);
+                    if(childObj == object) {
+                        field.set(clone,clone);
+                    }
+                    else {
+                        field.set(clone,CopyObject(field.get(object)));
+                    }
+                }
+            }
+            return clone;
+        }
+        catch (Exception e) {
+            return  null;
+        }
+    }
+}
