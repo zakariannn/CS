9ca03b38be067cba1f7a07a5dd2e12350f3238ce
zakariannn <72627891+zakariannn@users.noreply.github.com>
2021-08-04 04:28:18 +0400

    "commit"

--- src/Homeworks/ReflMethod.java
+++ src/Homeworks/ReflMethod.java
@@ -6,41 +6,30 @@ import java.lang.reflect.Modifier;
 public class ReflMethod {
 
 
-    public static Object CopyObject(Object object)
-    {
-        try
-        {
+    public static Object CopyObject(Object object) {
+        try {
             Object clone = object.getClass().newInstance();
 
-            for(Field field : object.getClass().getDeclaredFields()) {
+            for (Field field : object.getClass().getDeclaredFields()) {
                 field.setAccessible(true);
 
-                if(field.get(object) == null || Modifier.isFinal(field.getModifiers()))
-                {
+                if (field.get(object) == null || Modifier.isFinal(field.getModifiers())) {
                     continue;
                 }
 
-                if(field.getType().isPrimitive()
+                if (field.getType().isPrimitive()
                         || field.getType().equals(String.class)
                         || field.getType().getSuperclass().equals(Number.class)
-                        || field.getType().equals(Boolean.class))
-                {
+                        || field.getType().equals(Boolean.class)) {
                     field.set(clone, field.get(object));
-                }
-                else {
-                    Object childObj = field.get(object);
-                    if(childObj == object) {
-                        field.set(clone,clone);
-                    }
-                    else {
-                        field.set(clone,CopyObject(field.get(object)));
-                    }
+                } else {
+                    field.set(clone, CopyObject(field.get(object)));
                 }
             }
+
             return clone;
-        }
-        catch (Exception e) {
-            return  null;
+        } catch (Exception e) {
+            return null;
         }
     }
 }
